321cbea54adc5cdab96fa4d5c1ac10e766f19b9e
elija <elija@DESKTOP-DCPJPRL.dhcp.nd.edu>
2018-09-02 22:17:33 -0400

    first commit

--- src/edu/nd/se2018/homework/hwk1/Question1.java
+++ src/edu/nd/se2018/homework/hwk1/Question1.java
@@ -1,10 +1,27 @@
 package edu.nd.se2018.homework.hwk1;
+/**
+ * Homework 2 Problem 1
+ * @author Elijah hager
+ *
+ */
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.Iterator;
 
 public class Question1 {
+
+	public int getSumWithoutDuplicates(int[] arr) {
+		HashSet<Integer> nums = IntStream.of(arr).boxed().collect(Collectors.toCollection(HashSet::new));
 		
-	public Question1(){}
-	
-	public int getSumWithoutDuplicates(int[] numbers){
-		return 0;	
+		int sum = 0;
+		Iterator<Integer> it = nums.iterator();
+	    while(it.hasNext()){
+	        sum += it.next();
+	    }
+	    
+	    System.out.println(sum);
+		return sum;
 	}
+	
 }
--- src/edu/nd/se2018/homework/hwk1/Question3.java
+++ src/edu/nd/se2018/homework/hwk1/Question3.java
@@ -1,9 +1,47 @@
 package edu.nd.se2018.homework.hwk1;
+/**
+ * Homework 2 Problem 2
+ * @author Elijah hager
+ *
+ */
+
 public class Question3 {
 	
-	public Question3(){}	
-	
-    public int getMirrorCount(int[] numbers){
-		return 0;	
+	public int getMirrorCount(int[] arr) {
+		int[] reversedArr = mirrorArr(arr);
+		
+		return maxCount(arr, reversedArr);
 	}
+	
+	private int[] mirrorArr(int[] arr) {
+		int[] newArray = new int[arr.length];
+
+		for (int i = arr.length-1, j = 0; i >= 0; i--, j++) {
+			newArray[j] = arr[i];
+		}
+		
+		return newArray;
+	}
+	
+	private int maxCount(int[] forward, int[] reversed) {
+		int maxCount = 0;
+		int count = 0;
+
+		for (int i = 0; i < forward.length; i++) {
+			
+			if (forward[i] == reversed[i]) {
+				count++;
+			}else {
+				count = 0;
+			}
+			
+			if (count > maxCount) {
+				maxCount = count;
+			}
+			
+		  }
+		System.out.println(maxCount);
+		return maxCount;
+	}
+
 }
