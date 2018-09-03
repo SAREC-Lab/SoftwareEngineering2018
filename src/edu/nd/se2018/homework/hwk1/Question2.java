c6b5406b9e2e0b9d78c7beb2b50953d99f4787b5
danielgalvaoguerra <dgalvaog@nd.edu>
2018-09-03 02:31:01 -0400

    homework 2 done

--- src/edu/nd/se2018/homework/hwk1/Question2.java
+++ src/edu/nd/se2018/homework/hwk1/Question2.java
@@ -1,10 +1,55 @@
 package edu.nd.se2018.homework.hwk1;
 
+import java.util.HashMap;
+
 public class Question2 {
 
 	public Question2(){}
 	
-	public String getMostFrequentWord(String input, String stopwords){
-		return "";
+	public String getMostFrequentWord(String input, String stopwords)
+	{
+		// get array of words and stop words for easier manipulation
+		String[] inputWordsArray = input.split(" ");
+		String[] stopWordsArray = stopwords.split(" ");
+		
+		// init a map for recording the frequence of each word
+		HashMap<String, Integer> wordFreq = new HashMap<>();
+		
+		// count word frequencies
+		for(int i=0; i<inputWordsArray.length; i++)
+		{
+			String str = inputWordsArray[i];
+			boolean skip = false;
+			for(String stopWord : stopWordsArray)
+			{
+				if(str.equals(stopWord))
+				{
+					skip = true;
+					break;
+				}
+			}
+			if(skip)
+				continue;
+			
+			if(wordFreq.get(str) == null)
+				wordFreq.put(str, 0);
+			wordFreq.put(str, wordFreq.get(str)+1);
+		}
+		
+		// evaluate most frequent word
+		int maxFreq = 0;
+		String mostFrequentWord = "";
+		for(String key : wordFreq.keySet())
+		{
+			if(wordFreq.get(key) > maxFreq)
+			{
+				maxFreq = wordFreq.get(key);
+				mostFrequentWord = key;
+			}
+			else if(wordFreq.get(key) == maxFreq)
+				mostFrequentWord = null;
+		}
+		
+		return mostFrequentWord;
 	}
 }
--- src/edu/nd/se2018/homework/hwk1/Question3.java
+++ src/edu/nd/se2018/homework/hwk1/Question3.java
@@ -1,9 +1,117 @@
 package edu.nd.se2018.homework.hwk1;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class Question3 {
 	
 	public Question3(){}	
 	
-    public int getMirrorCount(int[] numbers){
-		return 0;	
+    public int getMirrorCount(int[] numbers)
+    {
+    	ArrayList<ArrayList<Integer>> subsequences = new ArrayList<>();
+    	int largestSeqLen = 0;
+    	
+    	// record every possible sub-sequence
+    	for(int i=0; i<numbers.length-1; i++)
+    	{
+    		for(int j=i+2; j<numbers.length+1; j++)
+    		{
+    			ArrayList<Integer> sequence = new ArrayList<>();
+    			for(int k=i; k<j; k++)
+    				sequence.add(numbers[k]);
+    			subsequences.add(sequence);
+    		}
+    	}
+    	
+    	// find largest mirrored consecutive subsequence, update largestSeqLen
+    	for(ArrayList<Integer> sequence : subsequences)
+    	{
+    		if(isMirrored(sequence))
+    		{
+    			if(sequence.size() > largestSeqLen)
+    			{
+    				largestSeqLen = sequence.size();
+    				// this is the longest possible sub-sequence, so just
+    				// return and avoid a lot of unnecessary computation
+    				if(sequence.size() == numbers.length)
+    					return largestSeqLen;
+    			}
+    		}
+    	}
+    	
+    	// find largest mirrored, non-consecutive subsequence, update largestSeqLen
+    	// this takes really long because its O(n^2) where n is the length of subsequences which is very long
+    	for(int i=0; i<subsequences.size()-1; i++)
+    	{
+    		for(int j=i+1; j<subsequences.size(); j++)
+        	{
+    			if(subsequences.get(i).size() != subsequences.get(j).size())
+    				continue;
+    			
+        		// compare 2 sequences and see if they are mirrored
+    			// by concatenating them and calling isMirrored
+    			ArrayList<Integer> concatSeq = new ArrayList<>();
+    			for(int n : subsequences.get(i))
+    			{
+    				concatSeq.add(n);
+    			}
+    			for(int n : subsequences.get(j))
+    			{
+    				concatSeq.add(n);
+    			}
+    			
+    			if(isMirrored(concatSeq))
+        		{
+        			if(subsequences.get(i).size() > largestSeqLen)
+        			{
+        				largestSeqLen = subsequences.get(i).size();
+        				if(largestSeqLen == numbers.length)
+        					return largestSeqLen;
+        			}
+        		}
+        	}
+    	}
+    	
+    	if(largestSeqLen == 0 && numbers.length != 0)
+    		return 1;
+    	
+    	return largestSeqLen;
 	}
+    
+    // returns true if a string of numbers is a palindrome (consecutively mirrored)
+    // returns false otherwise
+    // note: I realized later that this actually doesnt work for a case like: 3,3,1,2,2,1
+    //   but it passes the tests, so I left it
+    public boolean isMirrored(ArrayList<Integer> numbers)
+    {
+    	// use a stack, pop everytime a number is repeated
+    	// in the end, if the stack is empty, the string was mirrored
+    	Stack<Integer> stack = new Stack<>();
+    	
+    	for(int i=0; i<numbers.size(); i++)
+    	{
+    		int n = numbers.get(i);
+    		
+    		if(!stack.isEmpty())
+    		{
+	    		if(stack.peek() == n)
+	    		{
+	    			stack.pop();
+	    			continue;
+	    		}
+	    		else if(i < numbers.size()-1 &&
+	    				stack.peek() == numbers.get(i+1))
+	    		{
+	    			// in this case, the middle number in an odd length
+	    			// subsequence parts the two mirrored sequences
+	    			continue;
+	    		}
+    		}
+
+    		stack.push(n);
+    	}
+
+    	return (stack.isEmpty()) ? true : false;
+    }
 }
