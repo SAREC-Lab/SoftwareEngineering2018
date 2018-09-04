5eab6cc7dd2a1099cd733b509864841334ede21d
DJ Chao <37057165+DJ-Chao@users.noreply.github.com>
2018-09-04 15:55:18 -0400

    Update Question1.java

--- src/edu/nd/se2018/homework/hwk1/Question1.java
+++ src/edu/nd/se2018/homework/hwk1/Question1.java
@@ -1,33 +1,19 @@
 package edu.nd.se2018.homework.hwk1;
 
+import java.util.HashSet;
+
 public class Question1 {
 		
 	public Question1(){}
 	
 	public int getSumWithoutDuplicates(int[] numbers){
 		int sum = 0;
-		int[] checked = new int[arr.length];
+		HashSet<Integer> checked = new HashSet<Integer>();
 
-		for (int x = 0; x < arr.length; x++)
+		for (int x = 0; x < numbers.length; x++)
 		{
-			if (arr[x] != 0)
-			{
-				int checkCount = 0;
-				boolean flagged = false;
-				while (checkCount != arr.length && checked[x] != 0) 
-				{
-					if (checked[checkCount] == arr[x]) 
-					{
-						flagged = true;
-						break;
-					}
-					
-					checkCount++;
-				}
-				
-				if (flagged == false)
-					sum += arr[x];
-			}
+			if (checked.add(numbers[x]))
+				sum += numbers[x];
 		}
 		
 		return sum;
