79059dbe9030d6477faf11e3e1db501743f05094
DJ Chao <37057165+DJ-Chao@users.noreply.github.com>
2018-09-04 15:56:55 -0400

    Fixed Environment

--- src/edu/nd/se2018/homework/hwk1/Question3.java
+++ src/edu/nd/se2018/homework/hwk1/Question3.java
@@ -6,17 +6,26 @@ public class Question3 {
     public int getMirrorCount(int[] numbers){
 		int front = 0;
 		int back = numbers.length - 1;
+		
+		if (numbers.length == 0)
+			return 0;
+		
 		int mirrorCount = 0;
 		
 		while (front < numbers.length && back >= 0)
 		{
 			if (numbers[front] == numbers[back])
 				mirrorCount++;
+			else
+				break;
 			
 			front++;
 			back--;
 		}
 		
-		return mirrorCount;	
+		if (mirrorCount == 0)
+			return 1;	
+		else
+			return mirrorCount;
 	}
 }
