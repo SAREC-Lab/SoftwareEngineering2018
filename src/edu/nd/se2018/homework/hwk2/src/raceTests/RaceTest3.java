4aec813164301ff68eee679d9ad10307531d72b0
sjohns38 <42919971+sjohns38@users.noreply.github.com>
2018-09-10 16:45:39 -0400

    Update RaceTest3.java

--- src/edu/nd/se2018/homework/hwk2/src/raceTests/RaceTest3.java
+++ src/edu/nd/se2018/homework/hwk2/src/raceTests/RaceTest3.java
@@ -4,12 +4,12 @@ import org.junit.Test;
 public class RaceTest3 {
 	@Test
 	public void test() throws InterruptedException {
-		Race race1 = new Race();
-		race1.enrollHorse("Jimmy", 43, "steady");
-		race1.enrollHorse("klien", 46, "slow");
-		race1.enrollHorse("Dabo", 45, "early");
-		race1.enrollHorse("Sheen", 42, "slow");
-		race1.enrollHorse("Biscuit", 41, "steady");
+		Race race = new Race();
+		race.enrollHorse("Lily", 41, "steady");
+		race.enrollHorse("Argo", 42, "slow");
+		race.enrollHorse("Jackie", 45, "early");
+		race.enrollHorse("Stone", 46, "slow");
+		race.enrollHorse("Caleb", 41, "steady");
 	}	
 }
 
