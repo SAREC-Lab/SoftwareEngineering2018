502dfd00d76a09b6c2030598c6d0e2d109f7319c
Paul Kwak <pkwak1@nd.edu>
2018-09-17 00:08:05 -0400

    steps 1 -3?

--- src/edu/nd/se2018/homework/ColumbusGame/OceanExplorer.java
+++ src/edu/nd/se2018/homework/ColumbusGame/OceanExplorer.java
@@ -1,7 +1,30 @@
 package edu.nd.se2018.homework.ColumbusGame;
 
-public class OceanExplorer extend Application{
+import javafx.application.Application;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+public class OceanExplorer extends Application{
+	
+	Pane root;
+	
 	public static void main(String [ ] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage oceanStage) throws Exception {
+		// TODO Auto-generated method stub
+		// Step 2b
+		root = new Pane();
+		OceanMap oceanMap = new OceanMap();
+		int scale = 5;
+		// Step 2c
+		Scene scene = new Scene(root, 800, 800);
+		oceanStage.setScene(scene);
+		oceanStage.setTitle("Ship Explorer");
 		
+		oceanStage.show();
 	}
 }
--- src/edu/nd/se2018/homework/ColumbusGame/OceanMap.java
+++ src/edu/nd/se2018/homework/ColumbusGame/OceanMap.java
@@ -1,5 +1,25 @@
 package edu.nd.se2018.homework.ColumbusGame;
 
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
 public class OceanMap {
 
+	boolean[][] oceanGrid = new boolean[25][25];
+	final int dimensions = 25;
+	
+	public void drawMap(ObservableList<Node> root, int scale) {
+		for(int x = 0; x < dimensions; x++) {
+			for(int y = 0; y < dimensions; y++) {
+				Rectangle rect = new Rectangle(x * scale, y * scale, scale, scale);
+				rect.setStroke(Color.BLACK);
+				rect.setFill(Color.PALETURQUOISE);
+				root.add(rect);
+				oceanGrid[x][y] = false;
+			}
+		}
+	}
+	
 }
